a54a78697ee191a9e937a3fe21eaa9a337d3f18f
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-05-14 15:10:23 +0600

    database added

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = "mongodb+srv://doctorsuser:<password>@cluster0.4g4hk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const serviceCollection = client.db("doctors").collection("services");
+    // perform actions on the collection object
+    console.log('database connected');
+    client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hello from doctors');
