74dd19ba356ea75356171d22545b50955bc57452
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-05-19 20:27:12 +0600

    admin api added

--- index.js
+++ index.js
@@ -50,6 +50,16 @@ async function run() {
             res.send({ result, token });
         })
 
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
         app.get('/users', verifyJWT, async (req, res) => {
             const users = await userCollection.find().toArray();
             res.send(users);
