2e90f8c9d1a29ecd4fa2bc848146bf8aa8689dce
abdullaalfahad <abdullaalfahad.cse@gmail.com>
2022-05-20 15:11:55 +0600

    admin can add admin

--- index.js
+++ index.js
@@ -52,12 +52,20 @@ async function run() {
 
         app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email };
-            const updateDoc = {
-                $set: { role: 'admin' },
-            };
-            const result = await userCollection.updateOne(filter, updateDoc);
-            res.send(result);
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            console.log(email, requesterAccount);
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden' });
+            }
         })
 
         app.get('/users', verifyJWT, async (req, res) => {
